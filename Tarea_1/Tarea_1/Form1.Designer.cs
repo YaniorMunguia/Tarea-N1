22d44c0b7fc3064c5ba8c90e1e855e18250af1f9
YaniorMunguia <111203108+YaniorMunguia@users.noreply.github.com>
2023-02-03 21:42:32 -0600

    tarea 1

---
+++ Tarea_1/Tarea_1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Tarea_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b1 = new System.Windows.Forms.Button();
+            this.tx1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.b1.Location = new System.Drawing.Point(33, 43);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(193, 30);
+            this.b1.TabIndex = 5;
+            this.b1.Text = "Mostrar";
+            this.b1.UseVisualStyleBackColor = false;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // tx1
+            // 
+            this.tx1.Location = new System.Drawing.Point(195, 17);
+            this.tx1.Name = "tx1";
+            this.tx1.Size = new System.Drawing.Size(49, 20);
+            this.tx1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Tan;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese un Numero Entero:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 92);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.tx1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PAR O IMPAR - NEGATIVO O POSITIVO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.TextBox tx1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Tarea_1/Tarea_1/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+            int n = 0;
+            n = Convert.ToInt32(tx1.Text);
+
+            if (n % 2 == 0 & n >= 0)
+            {
+                MessageBox.Show("El número: " + n + "  Es un número Par - Positivo");
+
+            }
+            else if (n % 2 == 0 & n <= 0)
+            {
+                MessageBox.Show("El número : " + n + " Es un número Par - Negativo");
+
+            }
+            else if (n % 2 != 0 & n >= 0)
+            {
+                MessageBox.Show("El número : " + n + " Es un número Impar - Positivo");
+
+            }
+            else
+                MessageBox.Show("El número : " + n + " Es un número Impar - Negativo");
+
+        }
+
+    }
+}
+
